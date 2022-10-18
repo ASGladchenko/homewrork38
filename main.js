6bfa12774fd8b67f66c740abdadd045b22020f04
ASGladchenko <asgladchenko@gmail.com>
2022-10-18 14:47:58 +0300

    Homework 41

--- main.js
+++ main.js
@@ -1,49 +1,14 @@
-
-
-const userData = {
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {
-            "lat": "-37.3159",
-            "lng": "81.1496"
-        }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-    }
-};
-
-const renderUser = ({
-                        id,
-                        name,
-                        username,
-                        email,
-                        address: {street, suite, city, zipcode, geo: {lat, lng}},
-                        phone,
-                        website,
-                        company: {name: companyName, catchPhrase, bs}
-                    }) => {
-    let render = `
-        <div>
-            <h1>Homework 40</h1>
-            <p> <span>Users ID : ${id}</span> <span>Name : ${name} ,  NickName : ${username}  </span></p>
-            <p>Email : ${email}, Phone number : ${phone}, Website : ${website}</p>
-            <p>Work in ${companyName}, Business structure : ${bs}, Catch Phrase : ${catchPhrase} </p>
-            <p>Lives in : ${city}, ${street}, ${suite}, Zipcode : ${zipcode}  </p>
-            <p>Geolocation : lat :${lat} lng :${lng} </p>
-        </div>
-   `
-    document.querySelector('.container').insertAdjacentHTML("afterbegin", render)
+const myObjectAssign = (...rest) => {
+    let result ={}
+    rest.forEach((el) => {
+        result =({
+            ...result,
+            ...el,
+        })
+    })
+    return result
 }
-renderUser(userData);
+
+console.log(myObjectAssign({a: 1})); // { a: 1 }
+console.log(myObjectAssign({a: 1}, {b: 2})); // { a: 1, b: 2 }
+console.log(myObjectAssign({a: 1, b: 2}, {b: 3}, {a: 4, c: 5})); // { a: 4, b: 3, c: 5 }
