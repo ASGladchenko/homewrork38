95718b372be25c8c2dec054b65a91aa057cb04d6
ASGladchenko <asgladchenko@gmail.com>
2022-10-18 14:46:27 +0300

    Homework 40

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
-const sumAllNumbers = (...rest) => {
-    let result = 0;
-    for (const restElement of rest) {
-        if (typeof (restElement) !== 'number') return `Error! You can use only numbers.`
-        result += restElement
+
+
+const userData = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
     }
-    return result
+};
+
+const renderUser = ({
+                        id,
+                        name,
+                        username,
+                        email,
+                        address: {street, suite, city, zipcode, geo: {lat, lng}},
+                        phone,
+                        website,
+                        company: {name: companyName, catchPhrase, bs}
+                    }) => {
+    let render = `
+        <div>
+            <h1>Homework 40</h1>
+            <p> <span>Users ID : ${id}</span> <span>Name : ${name} ,  NickName : ${username}  </span></p>
+            <p>Email : ${email}, Phone number : ${phone}, Website : ${website}</p>
+            <p>Work in ${companyName}, Business structure : ${bs}, Catch Phrase : ${catchPhrase} </p>
+            <p>Lives in : ${city}, ${street}, ${suite}, Zipcode : ${zipcode}  </p>
+            <p>Geolocation : lat :${lat} lng :${lng} </p>
+        </div>
+   `
+    document.querySelector('.container').insertAdjacentHTML("afterbegin", render)
 }
-console.log(sumAllNumbers(1)) // 1
-console.log(sumAllNumbers(1, 2, 3)) // 6
-console.log(sumAllNumbers(1, 2, 3, 1, 2, 1, 10)) // 20
-console.log(sumAllNumbers(1, '2', 3)) // Error! You can use only numbers.
-console.log(sumAllNumbers([1, 2])) // Error! You can use only numbers.
-console.log(sumAllNumbers({number: 1})) // Error! You can use only numbers.
+renderUser(userData);
